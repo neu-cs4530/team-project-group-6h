182c49d22903ce27b66ffe7563c6ed5f0631ced6
chanmioh <chanmioh@gmail.com>
2022-03-24 18:40:46 -0400

    Created RecreationArea, MafiaGame, and RecreationPlayer types. Created functions for creating Mafia games and removing players from Mafia games.

---
+++ services/townService/src/lib/mafia_lib/MafiaGame.ts
@@ -0,0 +1,21 @@
+import { RecreationPlayer } from "./RecreationPlayer";
+
+/**
+  * Represents all the possible phases of a Mafia game.
+  */
+ export enum phase {
+    'lobby',
+    'discussion',
+    'voting',
+    'day',
+    'night',
+  };
+
+/**
+ * Represents type of MafiaGame that can be instantiated by players in a Recreation Room.
+ */
+ export type MafiaGame = {
+    players: RecreationPlayer[], // players in the game
+    phase: phase, 
+    isGameOver: boolean,
+}
