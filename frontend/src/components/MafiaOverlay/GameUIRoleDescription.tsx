882dbed570dd4cebccb845b6ab7f81a31cb9aa13
John Dunlevy <jdunlevy@Johns-MacBook-Pro.local>
2022-04-24 23:35:59 -0400

    add mafia members in role description for mafia roles

--- frontend/src/components/MafiaOverlay/GameUIRoleDescription.tsx
+++ frontend/src/components/MafiaOverlay/GameUIRoleDescription.tsx
@@ -1,29 +1,52 @@
 import { Container, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
-import { Role, Team } from '../../classes/GamePlayer';
+import GamePlayer, { Role, Team } from '../../classes/GamePlayer';
 
 type GameUIRoleDescriptionProps = {
   playerRole: Role | undefined;
   playerRoleInfo: string;
   playerTeam: Team | undefined;
+  gamePlayers: GamePlayer[];
 };
 
 function GameUIRoleDescription({
   playerRole,
   playerRoleInfo,
   playerTeam,
+  gamePlayers,
 }: GameUIRoleDescriptionProps): JSX.Element {
   return (
     <Container width='200px' height='296px' className='ui-container'>
       <Heading fontSize='xl' as='h1'>
         My Role:
         <br />
-        {playerRole ? Role[playerRole] : `General ${Team[playerTeam || Team.Unassigned]}`}
+        {playerRole ? Role[playerRole] : `General ${Team[playerTeam || Team.Mafia]}`}
       </Heading>
       <Text fontSize='md'>
-        <br />
-        Abilities: <br /> {playerRoleInfo}
+        Abilities: <br /> <Text fontSize='sm'>{playerRoleInfo}</Text>
       </Text>
+      {playerTeam === Team.Mafia ? (
+        <div>
+          <Heading fontSize='sm' as='h1'>
+            Mafia Members:
+          </Heading>
+          <ul>
+            {gamePlayers.map(p =>
+              p.team === Team.Mafia ? (
+                <li key={p.id}>
+                  <Text fontSize='sm'>
+                    {p.userName}: {Role[p.role]}
+                  </Text>
+                </li>
+              ) : (
+                <></>
+              ),
+            )}
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
     </Container>
   );
 }
